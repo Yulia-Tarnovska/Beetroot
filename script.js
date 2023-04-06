eb553eb54b854e1403aafabf9f6ab6e1b3100450
Yulia-Tarnovska <yulya5891@gmail.com>
2023-04-06 23:07:13 +0300

    lesson-29

---
+++ script.js
@@ -0,0 +1,34 @@
+// const new;
+// var null;
+/*const 01;
+let true;
+const int;
+const const
+*/
+
+const name1 = 'Bred Pit';
+const name2 = 'Angelina Jolie';
+const name3 = 'Penelope Cruz';
+const name4 = 'Robert Downey Jr';
+
+
+const name = prompt("Enter your name");
+alert(`Hello, ${name}`);
+
+const current_year = 2023;
+const birth_year = prompt("Enter year of birth");
+alert(`Age: ${current_year - birth_year}`);
+
+const square_side = prompt("Enter square side value");
+alert(`Square perimeter: ${square_side * 4}`);
+
+const circle_radius = prompt("Enter circle radius value");
+alert(`Circle area: ${Math.PI * Math.pow(Number(circle_radius), 2)}`);
+
+const distance = prompt("Enter distance value");
+const duration = prompt("Enter duration");
+alert(`Speed to be on time: ${distance / duration}`);
+
+const exchange_rate = 0.9;
+const dollar_amaunt = prompt("Enter dollar amount to exchange");
+alert(`Amount of euros: ${dollar_amaunt * exchange_rate}`);
